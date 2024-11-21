6a0cd910361a9cbb4166990cce1afba18dc016c6
Douglas Lim <douglas.lim.yy@gmail.com>
2024-11-21 21:42:51 +0800

    Update NFT

--- LoanEasy/NFT/NFT.sol
+++ LoanEasy/NFT/NFT.sol
@@ -4,20 +4,9 @@ pragma solidity ^0.8.0;
 import './ERC721.sol';
 
 contract NFT is ERC721 {
-    address admin;
-
-    constructor() {
-        admin = msg.sender;
-    }
-
-    // ensure function is only callable by the admin 
-    modifier adminOnly() {
-        require(msg.sender == admin, "You are not allowed to call this function as you are not the admin.");
-        _;
-    }
 
     // mint and transfer 
-    function generateReward(address _to) public adminOnly {
+    function generateReward(address _to) public {
         uint256 tokenId = mintToken();
         transferToken(tokenId, _to);
     }
